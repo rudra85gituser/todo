77109584bed39fcfb074b337609bb27455666678
Rudra Yadav <realrudrayadav@gmail.com>
2024-09-09 05:07:28 +0530

    todo

--- frontend-todo-app/src/App.jsx
+++ frontend-todo-app/src/App.jsx
@@ -1,110 +1,94 @@
-import {useState} from 'react'
-import './App.css'
-
-/*this is a state(the elements/data in ui which can be changed)
-let todo = {
-  title: "go to the gym",
-  description: "go at 5pm",
-  id:1
-}
-*/
-
-
-//now we can make many todos with different values but
-//but then react get confused which state it have to update , remove , add ,delete
-//so we have to define the data/input data/values in a specific way
-
-
-
-//this is a state(the elements in ui which can be changed)
-function App()
-{
-const [todo , setTodo] = useState({
-  title: "go to the gym",
-  description: "go at 5pm",
-  id:1
-});
-
-
-setInterval(()=>{
-  //this is a state(the elements in ui which can be changed)
-  //we havev to call set todo function whenever we are doing changes in the todo
-  setTodo({
-    title: " not go to the gym",
-    description: "not go at 5pm",
-    id:1
-  })
-},1000);
-
-
-//this is a component
-  return(
-  <div>
-  <h3>hi there this is my first react code</h3>
-  {todo.title}
-  {todo.description}
-  {todo.id}
-  <PersonName firstName={"rudra"} lastName={"yadav"}></PersonName>
-  </div>
-  )
-
-
-
-
-//new component
-function PersonName(props)
-{
-  return 
-  <div>
-  {props.firstName} {props.lastName}
-  </div>
-}
-}
-export default App
-
-
-
-
-
-
-
-//below code is predefined
-//below code is of ----This code is a basic React application that uses Vite as its build tool. It sets up a simple web page with a counter button and links to the Vite and React documentation. Here's a breakdown of what the code does:
-
-
-/*import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // State for todos and a counter
+  const [todos, setTodo] = useState([
+    {
+      title: "Go to the gym",
+      description: "Hit gym at 5-7",
+      id: 1,
+    },
+    {
+      title: "Go to dinner",
+      description: "Dinner at 7-9",
+      id: 2,
+    },
+  ]);
+  
+  const [counter, setCounter] = useState(Math.random());
+  var nonStateVariable = Math.random();
+
+  console.log("Todos:", todos);
+  console.log("Counter:", counter);
+  console.log("Non-state variable:", nonStateVariable);
+
+  
+//in this hook use effect the logic runs only one time
+React.useEffect(()=>{
+    console.log("hii from useEffect");
+},[]);
+
+  // To prevent unnecessary re-rendering, use useEffect for side effects like setTimeout
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTodo([
+        {
+          title: "Do not go to the gym " + Math.random(),
+          description: "Hit gym from 7-9",
+          id: 1,
+        },
+        {
+          title: "do not Have dinner " + Math.random(),
+          description: "Dinner from 7-9",
+          id: 2,
+        },
+      ]);
+    }, 10000);
+
+    return () => clearTimeout(timer); // Cleanup the timer on component unmount
+  }, []); // Empty dependency array to run once on mount
+
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {/* Displaying todos with JSON stringify */}
+      {JSON.stringify(todos)}
+
+      {/* Displaying each todo manually */}
+      <p>{todos[0].title}</p>
+      <p>{todos[0].description}</p>
+      <br />
+      <p>{todos[1].title}</p>
+      <p>{todos[1].description}</p>
+
+      {/* Displaying todos using map */}
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          <p>{todo.title}</p>
+          <p>{todo.description}</p>
+          <br />
+        </div>
+      ))}
+
+      {/* Displaying todos using props */}
+      {todos.map((todo) => (
+        <Todo key={todo.id} title={todo.title} description={todo.description} />
+      ))}
+    </div>
+  );
 }
 
-export default App
-*/
+// Todo component defined outside of the App component
+function Todo(props) {
+  return (
+    <div>
+      <p>{props.title}</p>
+      <p>{props.description}</p>
+      <br />
+    </div>
+  );
+}
+
+export default App;
--- frontend-todo-app/src/example2.jsx
+++ frontend-todo-app/src/example2.jsx
@@ -3,73 +3,7 @@ import React from "react";
 import {useState} from 'react'
 import fs from "fs"
 
-
-//1
-//understanding state and conponents
-
-
-
-//this is state
-function App()
-{
-    const [todoForToday , setTodoForToday] = React.useState({
-        title: "go to the gym" , 
-        description: "hit the gym at 5pm",
-        id: 1
-    }
-)
-
-
-setInterval(()=>{
-    setTodoForToday({
-        title: " not -> go to the gym",
-        description: "not -> go to eat 5pm",
-        id:1
-      })
-    },2000)
-
-
-    //wee are returnning component
-    //we are rendering the data
-return 
-{
-    <div>
-    {todoForToday.title}
-    <br/>
-    {todoForToday.description}
-    <br/>
-    {todoForToday.id}
-    </div>
-}
-
-return 
-{
-    <div>
-        <RenderTodo todoForToday={todoForToday}></RenderTodo>
-    </div>
-}
-}
-
-//we can pass satte variables to functions/components using props
-
-function RenderTodo(props)
-{
-    return
-    {
-    <div>
-    {props.todoForToday.title}
-    <br/>
-    {props.todoForToday.description}
-    <br/>
-    {props.todoForToday.id}
-    </div>
-
-    }
-}
-export default App;
-
-
-
+//when we have to pass list of elements and data to render or to display then we use array and maps 
 //2
 //rendering arrays
 //maps and filters
@@ -79,6 +13,8 @@ export default App;
 //doubling eack element of array or multiplying the each element by 2 using array
 let arr= [2,4,5,6];
 let newArr=[];
+
+//we can update the elements of the array after using const because the the elements points to a same location
 for(let i=0;i<arr.length;i++)
 {
      newArr.push(arr[i]*2);
@@ -87,10 +23,14 @@ console.log(newArr);
 
 
 //doing the same thing using maps
+//map is a internal function like push and pop
+//input we pass in map is function
 let newArr2 = arr.map((value)=>{
-    return value * 2
+    return value * 2;
 })
 console.log(newArr2);
+
+
 
 //or
 //without arrow function
@@ -99,11 +39,12 @@ function callback(value)
     console.log(value);
     return value*2;
 }
+//whatever function we are passsing in the map function  , it gets iterate for each element
 let newArr3 = arr.map(callback)
 console.log(newArr3);
 
 
-//when we have to pass list of elements and data to render or to display then we use array and maps
+
 //example 2
 // manipulating values using array and maps
 
@@ -143,3 +84,5 @@ const array2= map.array((value)=>
     })
 
     console.log(array2);
+
+    export default App;
